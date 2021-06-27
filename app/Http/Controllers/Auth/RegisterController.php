d74460b59b0e4ce1be5eb7e24d476b76a9ae1934
jshgwng <86574001+jshgwng@users.noreply.github.com>
2021-06-28 00:59:11 +0300

    initial commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        $rules = [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255','unique:users'],
+            'phone' => ['required', 'string', 'max:255'],
+            'type' => ['required'],
+            'password' => ['required', 'string', 'min:8', 'confirmed', 'regex:/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/',],
+            'email' => ['required', 'unique:users']
+        ];
+//        $messages = [
+//            'password' => 'Password and Confirmation do not match',
+//            'first_name' => 'The :attribute field is required.',
+//            'last_name' => 'The :attribute field is required.',
+//            'username' => 'The :attribute field is required.',
+//            'phone' => 'The :attribute field is required.',
+//            'type' => 'The :attribute field is required.',
+//        ];
+
+        return Validator::make($data, $rules);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'username' => $data['username'],
+            'phone' => $data['phone'],
+            'email' => $data['email'],
+            'type' => $data['type'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
---
+++ resources/views/menu/_menu.blade.php
@@ -0,0 +1,51 @@
+<header class="u-header">
+    <div class="u-header-left">
+        <a class="u-header-logo"href="">
+            <img src="{{asset('storage/images/main/LOGO.png')}}" alt="WillieScant" width="50px" height="55px">
+        </a>
+    </div>
+
+    <div class="u-header-right">
+
+        <a href="{{route('login')}}" class="link-muted nav-link">Login</a>
+        <a href="{{route('register')}}" class="link-muted nav-link">Sign up</a>
+
+        <!-- User Profile -->
+        <div class="dropdown ml-2">
+            <a class="link-muted d-flex align-items-center" href="#" role="button" id="dropdownMenuLink" aria-haspopup="true" aria-expanded="false" data-toggle="dropdown">
+                <img class="mr-2" width='25px' height='25px' src="{{asset('storage/images/main/services.png')}}" alt="User Profile" >
+            </a>
+
+            <div class='dropdown-menu dropdown-menu-right border-0 py-0 mt-4' aria-labelledby="dropdownMenuLink"
+                 style="width: 260px;">
+                <h4 class='text-center pt-2'> Our Services</h4>
+                <hr>
+                <div class='d-flex'>
+                    <a class='dropdown-item service' href="#"><img src="{{asset('storage/images/main/LOGO.png')}}" width='35' height='45'></a>
+                    <a class='dropdown-item service' href='https://housingsmart.co.ke/homepage'><img src="{{asset('storage/images/main/housing.Jpg')}}" width='35' height='45'></a>
+                </div>
+
+                <div class='d-flex'>
+                    <a class='dropdown-item service' href='#'>Health</a>
+                    <a class='dropdown-item service' href='#'>Food</a>
+                </div>
+
+                <div class='d-flex'>
+                    <a class='dropdown-item service' href='#'>Clothing</a>
+                    <a class='dropdown-item service' href='#'>Security</a>
+                </div>
+
+                <div class='d-flex'>
+                    <a class='dropdown-item service' href='#'>Education</a>
+                    <a class='dropdown-item service' href='#'>Transport</a>
+                </div>
+
+                <div class='d-flex'>
+                    <a class='dropdown-item service' href='#'>Innovation</a>
+                    <a class='dropdown-item service' href='#'>Health</a>
+                </div>
+            </div>
+        </div>
+        <!-- End User Profile -->
+    </div>
+</header>
